aea2bee642f7f42e1f632ebfe488d03d715e14aa
Azumabashi <>
2022-12-05 06:27:28 +0900

    move proc join

--- lib/syntaxChecker.nim
+++ lib/syntaxChecker.nim
@@ -1,18 +1,7 @@
 import types
 import sequtils
 import strutils
-
-proc join(contents: seq[Nimenta]): string = 
-  result = contents.map(
-    proc(content: Nimenta): string = 
-      if content.ctype == ContentType.text:
-        result = content.content
-      elif content.ctype == ContentType.group:
-        result = join(content.inGroup)
-      else:
-        echo "Syntax Error: env name should be contain only merely string and should not be contain other things (i,e., groups)."
-        quit(1)
-  ).join("")
+import utils
 
 proc syntaxChecker*(contents: seq[Nimenta]): bool = 
   var 
