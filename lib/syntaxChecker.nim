cb5e7cd04f739e3c5e33d7943957edb56220164a
Azumabashi <>
2022-12-04 16:22:42 +0900

    use stack to detect unmatch environment

--- lib/syntaxChecker.nim
+++ lib/syntaxChecker.nim
@@ -15,12 +15,33 @@ proc join(contents: seq[Nimenta]): string =
   ).join("")
 
 proc syntaxChecker*(contents: seq[Nimenta]): bool = 
-  var idx = 0
+  var 
+    idx = 0
+    envNameStack: seq[string] = @[]
   while idx < contents.len:
     case contents[idx].ctype
     of ContentType.command:
       case contents[idx].content
-      of r"\documentclass", r"\begin", r"\end", r"\textbf":
+      of r"\documentclass":
+        if idx == 0 and contents[idx + 1].ctype == ContentType.group:
+          assert syntaxChecker(contents[idx+1].inGroup)
+          idx += 2
+        else:
+          return false
+      of r"\begin", r"\end":
+        if idx == 0 and contents[idx + 1].ctype == ContentType.group:
+          assert syntaxChecker(contents[idx+1].inGroup)
+          if contents[idx].content == r"\begin":
+            envNameStack.add(contents[idx+1].inGroup.join)
+          else:
+            let nowGroup = envNameStack.pop()
+            if contents[idx+1].inGroup.join != nowGroup:
+              echo "Error: Environment does not match!!!"
+              return false
+          idx += 2
+        else:
+          return false
+      else:
         if contents[idx + 1].ctype == ContentType.group:
           assert syntaxChecker(contents[idx+1].inGroup)
           idx += 2
