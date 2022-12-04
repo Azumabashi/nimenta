f344853aaa1546b60015d321e8eaadae77f43c2f
Azumabashi <>
2022-12-04 09:28:19 +0900

    add proc syntaxParser

--- lib/syntaxParser.nim
+++ lib/syntaxParser.nim
@@ -1,7 +1,12 @@
 import strutils
+import types
 
 proc getContent(path: string): seq[string] = 
   var file = open(path, FileMode.fmRead)
   defer:
     close(file)
   result = file.readAll().split("\n")
+
+proc syntaxParser*(path: string): seq[Nimenta] = 
+  let contents = path.getContent
+  var parsed: seq[Nimenta] = @[]
